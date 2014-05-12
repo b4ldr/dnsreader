e218ef45ccad2be6db1509286a2049c047b4479d
John Bond <john.r.bond@gmail.com>
2014-05-12 06:35:48 -0400

    add support for yaml or zabbix

--- server.py
+++ server.py
@@ -14,13 +14,13 @@ import dns.rdatatype
 
 #class DnsReaderServer(SocketServer.ThreadingUDPServer):
 class DnsReaderServer(SocketServer.UDPServer):
-    """
+    '''
     SocketServer.ThreadingUDPServer 
 
     Instance variables:
     
     - RequestHandlerClass
-    """
+    '''
     def __init__(self,server_address,RequestHandlerClass, directory, zabbix_server):
         #SocketServer.ThreadingUDPServer.__init__(self,server_address,RequestHandlerClass)
         SocketServer.UDPServer.__init__(self,server_address,RequestHandlerClass)
@@ -30,9 +30,17 @@ class DnsReaderServer(SocketServer.UDPServer):
         self.zabbix_server = zabbix_server
 
 class DnsReaderHanlder(SocketServer.BaseRequestHandler):
-    """
-    Handeler class 
-    """
+    '''
+    Base Handeler class 
+    '''
+
+    message = None
+    nsid = None
+    serial = None
+    data = None
+    incoming = None
+    node_name = None
+
     def __init__(self, request, client_address, server):
         SocketServer.BaseRequestHandler.__init__(self, request, client_address, server)
 
@@ -45,6 +53,57 @@ class DnsReaderHanlder(SocketServer.BaseRequestHandler):
                 return qname
         else:
             return nsid
+       
+    def format_qname (self, qname):
+        if qname == '.':
+            return 'root'
+        else:
+            return qname[:-1]
+
+    def parse_dns(self):
+        '''
+        parse the data package into dns elements
+        '''
+        self.data = str(self.request[0]).strip()
+        self.incoming = self.request[1]
+        #incoming Data
+        try:
+            self.message = dns.message.from_wire(self.data)
+        except dns.name.BadLabelType:
+            #Error processing lable (bit flip?)
+            return False 
+        except dns.message.ShortHeader:
+            #Recived junk
+            return False
+        else:
+            current_time = int(time.time())
+            self.qname = self.format_qname(self.message.question[0].name.to_text())
+            for opt in self.message.options: 
+                    if opt.otype == dns.edns.NSID: 
+                        self.nsid = str(opt.data)
+                        if '.' not in self.nsid:
+                            self.nsid = self.nsid.decode("hex")
+            self.node_name = self.set_node_name(self.nsid)
+            for ans in self.message.answer:
+                if ans.rdtype == dns.rdatatype.SOA:
+                    self.serial =  ans[0].serial
+            print "%s: %s %s @%s" % (self.nsid, self.qname, self.serial, current_time)
+            return True
+
+    def handle(self):
+        '''
+        RequestHandlerClass handle function
+        handler listens for dns packets
+        '''
+        raise NotImplementedError('This needs to be implmented by a child class')
+
+class DnsReaderHanlderZabbix(DnsReaderHanlder):
+    '''
+    Handeler class with zabbix output 
+    '''
+    def __init__(self, request, client_address, server):
+        DnsReaderHanlder.__init__(self, request, client_address, server)
+         
     def _zabbix_recv_all(self, sock):
         buf = ''
         zbx_hdr_size = 13
@@ -66,20 +125,15 @@ class DnsReaderHanlder(SocketServer.BaseRequestHandler):
             zbx_sock.close()
             raise Exception(e[1])
         else:
-            #try:
             zbx_srv_resp_hdr = self._zabbix_recv_all(zbx_sock)
             zbx_srv_resp_body_len = struct.unpack('<Q', zbx_srv_resp_hdr[5:])[0]
             zbx_srv_resp_body = zbx_sock.recv(zbx_srv_resp_body_len)
             zbx_sock.close()
-            #except:
-            #    zbx_sock.close()
-            #    raise Exception("Error while sending data to Zabbix")
-
         return json.loads(zbx_srv_resp_body)
 
-    def send_zabbix_data(self, host, key, value):
+    def send_zabbix_data(self, key, value):
         data = [{
-            'host': host,
+            'host': self.node_name,
             'key': key,
             'value': value
             }]
@@ -87,96 +141,71 @@ class DnsReaderHanlder(SocketServer.BaseRequestHandler):
         message = 'ZBXD\1' + struct.pack('<Q',  len(body)) + body
         response = self._send_to_zabbix(message)
         print response['info']
-        
+    
+    def handle(self):
+        '''
+        RequestHandlerClass handle function
+        handler listens for dns packets
+        '''
+        if self.parse_dns():
+            if self.node_name:
+                if self.serial:
+                    zabbix_key = 'spoof_{}_serial'.format(self.qname.replace('.','_'))
+                    zabbix_value = self.serial
+                else:
+                    zabbix_key = 'spoof_nsid'
+                    zabbix_value = self.nsid
+                self.send_zabbix_data(zabbix_key, zabbix_value)
 
-    def format_qname (self, qname):
-        if qname == '.':
-            return 'root'
-        else:
-            return qname[:-1]
+class DnsReaderHanlderYaml(DnsReaderHanlder):
+    '''
+    Handeler class with yaml output 
+    '''
+    node_file = None
 
-    def write_yaml(self, node_name, nsid, qname, serial):
+    def __init__(self, request, client_address, server):
+        DnsReaderHanlder.__init__(self, request, client_address, server)
 
+    def write_yaml(self):
+        '''
+        write out yaml file for a specific node
+        '''
+        self.node_file = os.path.join(self.server.directory, "%s.yaml" % self.node_name)
         now = int(time.time())
-
-        node_file = os.path.join(self.server.directory, "%s.yaml" % node_name)
-
-        if os.path.exists(node_file):
-            node_doc = file(node_file, 'r+')
+        if os.path.exists(self.node_file):
+            node_doc = file(self.node_file, 'r+')
         else:
-            node_doc = file(node_file, 'w+')
-
+            node_doc = file(self.node_file, 'w+')
         node = yaml.load(node_doc)
-
         if not node:
-            node = { 'name': node_name, 'nsid': { 
+            node = { 'name': self.node_name, 'nsid': { 
                         'value': None, 
                         'updated': 0 },
                      'domains': { },
                     }
-
-        node['nsid']['value'] = nsid
-        if nsid:
+        node['nsid']['value'] = self.nsid
+        if self.nsid:
             node['nsid']['updated'] = now
-
-        if serial:
+        if self.serial:
             try:
-                node['domains'][qname]['serial'] = serial
-                node['domains'][qname]['updated'] = now
+                node['domains'][self.qname]['serial'] = self.serial
+                node['domains'][self.qname]['updated'] = now
             except KeyError:
-                node['domains'][qname] = { 'serial' : serial, 'updated' : now }
+                node['domains'][self.qname] = { 'serial' : self.serial, 'updated' : now }
         #rewrite the file
         node_doc.seek(0)
-        yaml.dump(node,node_doc, indent=4, default_flow_style = False)
+        yaml.dump(node, node_doc, indent=4, default_flow_style = False)
         node_doc.close()
-
-
-
+    
     def handle(self):
-            """
-            RequestHandlerClass handle function
-            handler listens for dns packets
-            """
+        '''
+        RequestHandlerClass handle function
+        handler listens for dns packets
+        '''
+        if self.parse_dns():
+            if self.node_name:
+                self.write_yaml()
 
-            #incoming Data
-            message = None
-            nsid = None
-            serial = None
-            data = str(self.request[0]).strip()
-            node_file = None
-            #Sending machine
-            incoming = self.request[1]
-            try:
-                message = dns.message.from_wire(data)
-                current_time = int(time.time())
-                qname = self.format_qname(message.question[0].name.to_text())
-                for opt in message.options: 
-                      if opt.otype == dns.edns.NSID: 
-                          nsid = opt.data
-                          if '.' not in nsid:
-                              nsid = nsid.decode("hex")
-                node_name = self.set_node_name(nsid)
-                for ans in message.answer:
-                    if ans.rdtype == dns.rdatatype.SOA:
-                        serial =  ans[0].serial
-                print "%s: %s %s @%s" % (nsid, qname, serial, current_time)
-                if node_name:
-                    if serial:
-                        zabbix_key = 'spoof_{}_serial'.format(qname.replace('.','_'))
-                        zabbix_value = serial
-                    else:
-                        zabbix_key = 'spoof_nsid'
-                        zabbix_value = nsid
-                    self.write_yaml(node_name, str(nsid), qname, serial)
-                    self.send_zabbix_data(node_name, zabbix_key, zabbix_value)
-                #if message.rcode() != dns.rcode.NOERROR:
-                #    print "%s: %s %s @%s" % (nsid, qname, serial, current_time)
-            except dns.name.BadLabelType:
-                #Error processing lable (bit flip?)
-                pass 
-            except dns.message.ShortHeader:
-                #Recived junk
-                pass
 
 def main():
     ''' main function for using on cli'''
@@ -184,9 +213,15 @@ def main():
     parser.add_argument('-l', '--listen', metavar="0.0.0.0:6969", default="0.0.0.0:6969", help='listen on address:port ')
     parser.add_argument('-z', '--zabbix-server', metavar="localhost:10051", default="localhost:10051", help='Zabbix trapper server')
     parser.add_argument('-d', '--directory', metavar="/tmp/dnsdata/", default="/tmp/dnsdata/", help='Directory to store node information')
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('--yaml', action='store_true')
+    group.add_argument('--zabbix', action='store_true')
     args = parser.parse_args()
     host, port = args.listen.split(":")
-    server = DnsReaderServer((host, int(port)), DnsReaderHanlder, args.directory, args.zabbix_server )
+    if args.yaml:
+        server = DnsReaderServer((host, int(port)), DnsReaderHanlderYaml, args.directory, args.zabbix_server )
+    elif args.zabbix:
+        server = DnsReaderServer((host, int(port)), DnsReaderHanlderZabbix, args.directory, args.zabbix_server )
     server.serve_forever()
 
 if __name__ == "__main__":
